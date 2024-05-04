d26c9e14f653f047bb201d9f1b32fc3c5a7668d7
Lucas Monesi <luca06msantiago@gmail.com>
2024-05-04 14:29:56 -0300

    crud de leves

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesWebMvc.Data;
+using System.Configuration;
 var builder = WebApplication.CreateBuilder(args);
+var connectionStr = "server=localhost;userid=developer;password=toby+5$;database=saleswebmvcappdb";
+builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+    options.UseMySql(connectionStr, ServerVersion.AutoDetect(connectionStr)));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
