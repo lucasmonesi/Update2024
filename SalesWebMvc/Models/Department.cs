59f2c3670ab642e4cb5602accea2d9849276edfa
Lucas Monesi <luca06msantiago@gmail.com>
2024-05-07 14:42:32 -0300

    isso ai

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,8 +1,31 @@
-﻿namespace SalesWebMvc.Models
+﻿using Microsoft.Identity.Client;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
